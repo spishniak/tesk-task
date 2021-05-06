88119735ed479817e978f33f705f086c3f4f47c4
ivan <spishniak@gmail.com>
2021-05-06 18:47:53 +0300

    gifs.ejs welcome.ejs

--- src/index.js
+++ src/index.js
@@ -13,15 +13,11 @@ app.get('/search', async (req, res) => {
     const searchQuery = req.query.search
     const giphyResponse = await giphyService.search(searchQuery)
     const gifs = giphyResponse.data.data.map(gif => gif.embed_url)
-    res.render('welcome.ejs', {
-        gifLink: "https://i.gifer.com/origin/ed/ed7843c794b34fe4fc95260a9231625a.gif",
-    })
+    res.render('gifs.ejs', { gifs })
 })
 
 app.use('/', (req, res) => {
-    res.render('welcome.ejs', {
-        gifLink: "https://i.gifer.com/origin/ed/ed7843c794b34fe4fc95260a9231625a.gif",
-    })
+    res.render('welcome.ejs')
 })
 
 
