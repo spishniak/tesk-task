58f5fcf4b15ecb294c97d6f104a7ce9b26938b83
ivan <spishniak@gmail.com>
2021-05-06 18:56:06 +0300

    version 0.1

--- src/components/giphy/giphy.repository.js
+++ src/components/giphy/giphy.repository.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const API_KEY = 'dZeBBfToBrL0yMY6U7KMQsZosu1BuUWl';
 
-function makeGiphyRequest(searchQuery) {
-    return axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchQuery}`)
+function makeGiphyRequest(searchQuery, limit = 10) {4
+    return axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchQuery}&limit=${limit}`)
 }
 
 export default {
