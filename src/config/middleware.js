997a8029ff2ffae83209cc27dfc3ccdae820a0a7
ivan <spishniak@gmail.com>
2021-05-06 09:44:21 +0300

    add welcome.ejs, app.get('search',...)

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -4,4 +4,16 @@ import morgan from 'morgan'
 function settingApplication(app) {
     app.use(express.json())
     app.use(morgan('dev'))
+    app.set("view engine", "ejs")
 }
+
+
+
+
+
+
+
+
+
+
+export default  settingApplication
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import express from 'express'
-
+import settingApplication from './config/middleware.js'
 
 const app = express()
+settingApplication(app)
 
 
 
 
 
+app.get('/search', (req, res) => {
+    const searchQuery = req.query
+    
+    res.render('welcome.ejs', {
+        gifLink: "https://i.gifer.com/origin/ed/ed7843c794b34fe4fc95260a9231625a.gif",
+    })
+})
 
-
+app.use('/', (req, res) => {
+    res.render('welcome.ejs')
+})
 
 
 
